ff8c2368375ebdc15d0ee18005dcff58a511c941
Ines-Fidalgo <40200308@esmad.ipp.pt>
2021-03-30 11:21:27 +0100

    Configuração inicial do jogo

--- Assets/Scripts/SpawnInvaders.cs
+++ Assets/Scripts/SpawnInvaders.cs
@@ -12,6 +12,9 @@ public class SpawnInvaders : MonoBehaviour
     GameObject invasorB;
 
     [SerializeField]
+    GameObject invasorC;
+
+    [SerializeField]
     int nInvasores = 7;
 
     [SerializeField]
@@ -27,8 +30,40 @@ public class SpawnInvaders : MonoBehaviour
         for( int i = 1; i <= nInvasores; i++ )
         {
             GameObject newInvader = Instantiate(invasorA, transform);
-            newInvader.transform.position = new Vector3(x, -0.5f, 0f);
+            newInvader.transform.position = new Vector3(x, -1.5f, 0f);
             x += 1f;
+        }
+
+        float x2 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorA, transform);
+            newInvader.transform.position = new Vector3(x2, -0.5f, 0f);
+            x2 += 1f;
+        }
+
+        float x3 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorB, transform);
+            newInvader.transform.position = new Vector3(x3, 0.5f, 0f);
+            x3 += 1f;
+        }
+
+        float x4 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorB, transform);
+            newInvader.transform.position = new Vector3(x4, 1.5f, 0f);
+            x4 += 1f;
+        }
+
+        float x5 = xMin;
+        for (int i = 1; i <= nInvasores; i++)
+        {
+            GameObject newInvader = Instantiate(invasorC, transform);
+            newInvader.transform.position = new Vector3(x5, 2.5f, 0f);
+            x5 += 1f;
         }
     }
 }
